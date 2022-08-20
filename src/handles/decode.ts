9403d911619407f806de5df44c7b7494e751ff81
tom <toms@live.jp>
2022-08-20 12:56:13 +0900

    decode() is added

---
+++ src/handles/decode.ts
@@ -0,0 +1,18 @@
+import { SecretKey } from "endecoder";
+
+type Params = {
+  encodedText: string;
+  password: string;
+  salt: string;
+};
+
+export function decode(
+  event: any,
+  { encodedText, password, salt }: Params
+) {
+  return SecretKey.activate({
+    password,
+    salt
+  })
+    .decode(encodedText);
+}
