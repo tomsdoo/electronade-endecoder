54ee5922aae05ff65452ba3a79d707ef68480508
tom <toms@live.jp>
2022-08-30 13:58:18 +0900

    test for handle

--- test/handles.test.ts
+++ test/handles.test.ts
@@ -1,10 +1,13 @@
 import { describe, it } from "mocha";
 import { strict as assert } from "assert";
 
+import { randomBytes } from "crypto";
+
 import { handles } from "../src/handles";
 
 let plainText: string;
 let password: string;
+let plainBuffer: Buffer;
 let salt: string;
 let handleStore: {
   [key: string]: Function;
@@ -14,6 +17,7 @@ describe("handles", () => {
   before(() => {
     plainText = "this is the plain text!123";
     password = "p@ssword!!";
+    plainBuffer = randomBytes(1024);
     salt = "salt//";
     handleStore = Object.fromEntries(
       handles.map(({ eventName, handler }) => [ eventName, handler ])
@@ -46,6 +50,35 @@ describe("handles", () => {
   it("electronade-endecoder:encrypt eventName exists", () => {
     assert(
       "electronade-endecoder:encrypt" in handleStore
+    );
+  });
+
+  it("electronade-endecoder:decrypt eventName exists", () => {
+    assert(
+      "electronade-endecoder:decrypt" in handleStore
+    );
+  });
+
+  it("electrolade-endecoder:encrypt and decrypt handlers", async () => {
+    const [
+      password,
+      salt
+    ] = [
+      randomBytes(36).toString("base64"),
+      randomBytes(12).toString("base64")
+    ];
+    const encodedText = await handleStore["electronade-endecoder:encrypt"]({}, {
+      plainBuffer,
+      password,
+      salt
+    });
+    assert.equal(
+      await handleStore["electronade-endecoder:decrypt"]({}, {
+        encodedText,
+        password,
+        salt
+      }).then((buffer: Buffer) => buffer.toString("base64")),
+      plainBuffer.toString("base64")
     );
   });
 });
