733d4d3daba8207c7b89fd7491880c091c9bc9ce
tom <toms@live.jp>
2022-09-28 18:50:18 +0900

    format:test

--- test/handles.test.ts
+++ test/handles.test.ts
@@ -20,64 +20,63 @@ describe("handles", () => {
     plainBuffer = randomBytes(1024);
     salt = "salt//";
     handleStore = Object.fromEntries(
-      handles.map(({ eventName, handler }) => [ eventName, handler ])
+      handles.map(({ eventName, handler }) => [eventName, handler])
     );
   });
 
   it("electronade-endecoder:encode eventName exists", () => {
-    assert(
-      "electronade-endecoder:encode" in handleStore
-    );
+    assert("electronade-endecoder:encode" in handleStore);
   });
 
   it("electronade-endecoder:decode eventName exists", () => {
-    assert(
-      "electronade-endecoder:decode" in handleStore
-    );
+    assert("electronade-endecoder:decode" in handleStore);
   });
 
   it("electronade-endecoder:encode and electronade-endecoder:decode handlers", () => {
-    const encodedText = handleStore["electronade-endecoder:encode"]
-      ({}, { plainText, password, salt });
+    const encodedText = handleStore["electronade-endecoder:encode"](
+      {},
+      { plainText, password, salt }
+    );
 
     assert.equal(
-      handleStore["electronade-endecoder:decode"]
-        ({}, { encodedText, password, salt }),
+      handleStore["electronade-endecoder:decode"](
+        {},
+        { encodedText, password, salt }
+      ),
       plainText
-    )
+    );
   });
 
   it("electronade-endecoder:encrypt eventName exists", () => {
-    assert(
-      "electronade-endecoder:encrypt" in handleStore
-    );
+    assert("electronade-endecoder:encrypt" in handleStore);
   });
 
   it("electronade-endecoder:decrypt eventName exists", () => {
-    assert(
-      "electronade-endecoder:decrypt" in handleStore
-    );
+    assert("electronade-endecoder:decrypt" in handleStore);
   });
 
   it("electrolade-endecoder:encrypt and decrypt handlers", async () => {
-    const [
-      password,
-      salt
-    ] = [
+    const [password, salt] = [
       randomBytes(36).toString("base64"),
-      randomBytes(12).toString("base64")
+      randomBytes(12).toString("base64"),
     ];
-    const encodedText = await handleStore["electronade-endecoder:encrypt"]({}, {
-      plainBuffer,
-      password,
-      salt
-    });
-    assert.equal(
-      await handleStore["electronade-endecoder:decrypt"]({}, {
-        encodedText,
+    const encodedText = await handleStore["electronade-endecoder:encrypt"](
+      {},
+      {
+        plainBuffer,
         password,
-        salt
-      }).then((buffer: Buffer) => buffer.toString("base64")),
+        salt,
+      }
+    );
+    assert.equal(
+      await handleStore["electronade-endecoder:decrypt"](
+        {},
+        {
+          encodedText,
+          password,
+          salt,
+        }
+      ).then((buffer: Buffer) => buffer.toString("base64")),
       plainBuffer.toString("base64")
     );
   });
