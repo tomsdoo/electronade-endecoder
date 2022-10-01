ff2f473d70fca77dfdedeef0388228f81040cbc3
tomsdoo <32782986+tomsdoo@users.noreply.github.com>
2022-10-02 06:17:42 +0900

    lint:src fix (#14)

--- src/index.ts
+++ src/index.ts
@@ -1,55 +1,64 @@
 export { handles } from "./handles";
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const { ipcRenderer } = require("electron");
 
 export const preloadObject = {
   endecoder: {
-    encode: ({
+    encode: async ({
       plainText,
       password,
-      salt
+      salt,
     }: {
       plainText: string;
       password: string;
       salt: string;
-    }) => ipcRenderer.invoke(
-      "electronade-endecoder:encode",
-      { plainText, password, salt }
-    ),
-    decode: ({
+    }) =>
+      await ipcRenderer.invoke("electronade-endecoder:encode", {
+        plainText,
+        password,
+        salt,
+      }),
+    decode: async ({
       encodedText,
       password,
-      salt
+      salt,
     }: {
       encodedText: string;
       password: string;
       salt: string;
-    }) => ipcRenderer.invoke(
-      "electronade-endecoder:decode",
-      { encodedText, password, salt }
-    ),
-    encrypt: ({
+    }) =>
+      await ipcRenderer.invoke("electronade-endecoder:decode", {
+        encodedText,
+        password,
+        salt,
+      }),
+    encrypt: async ({
       plainBuffer,
       password,
-      salt
+      salt,
     }: {
       plainBuffer: Buffer;
       password: string;
       salt: string;
-    }) => ipcRenderer.invoke(
-      "electronade-endecoder:encrypt",
-      { plainBuffer, password, salt }
-    ),
-    decrypt: ({
+    }) =>
+      await ipcRenderer.invoke("electronade-endecoder:encrypt", {
+        plainBuffer,
+        password,
+        salt,
+      }),
+    decrypt: async ({
       encodedText,
       password,
-      salt
+      salt,
     }: {
       encodedText: string;
       password: string;
       salt: string;
-    }) => ipcRenderer.invoke(
-      "electronade-endecoder:decrypt",
-      { encodedText, password, salt }
-    )
-  }
+    }) =>
+      await ipcRenderer.invoke("electronade-endecoder:decrypt", {
+        encodedText,
+        password,
+        salt,
+      }),
+  },
 };
